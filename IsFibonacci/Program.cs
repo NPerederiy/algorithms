44dcd0a5e3897433a749593a9a9ac7c5ecc5d0b0
NPerederiy <nick.perederiy@gmail.com>
2018-10-26 14:15:02 +0300

    Added algorithm #2
    Added an algorithm that determines whether a given number is a Fibonacci number

---
+++ IsFibonacci/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+namespace Fibonacci
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("_in: ");
+            var _in = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine(IsFibonacci(_in));
+            Console.ReadKey();
+        }
+
+        private static bool IsFibonacci(int _in)
+        {
+            var _prev = 0;
+            var _this = 0;
+            var _next = 1;
+
+            for(;;)
+            {
+                if (_next > _in)
+                    return false;
+                if (_in == _next)
+                    return true;
+                _prev = _this;
+                _this = _next;
+                _next = _this + _prev;
+            }
+        }
+    }
+}
--- README.md
+++ README.md
@@ -1,8 +1,6 @@
 # Algorithm List
-### GetLongestSequenceLength
-#### Description:
-The algorithm for finding the length of the longest continuous chain of ones in a sequence of zeros and ones
-#### Input:
-A sequence of zeros and ones
-#### Output:
-The length of the longest continuous chain of ones
+
+Name | Description | Input | Output
+-----| ----------- | ----- | ------
+GetLongestSequenceLength | The algorithm for finding the length of the longest continuous chain of ones in a sequence of zeros and ones | Sequence of zeros and ones | The length of the longest continuous chain of ones
+IsFibonacci | The algorithm determines whether a given number is a Fibonacci number | Number | Boolean value
