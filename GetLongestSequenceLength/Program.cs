44c27d7f265c68bb33083e137bb9dc7e0064daba
NPerederiy <nick.perederiy@gmail.com>
2018-10-26 12:17:44 +0300

    Added algorithm #1
    Added search algorithm for the longest continuous chain of ones in the sequence of zeros and ones

---
+++ GetLongestSequenceLength/Program.cs
@@ -0,0 +1,26 @@
+﻿using System.IO;
+
+namespace ZerosAndOnes
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var input = File.ReadAllText("INPUT.txt");
+
+            File.WriteAllText("OUTPUT.txt", GetLongestSequenceLength(input).ToString());
+        }
+
+        private static int GetLongestSequenceLength(string input)
+        {
+            var maxnum = 0;
+            var sequences = input.Split('0');
+
+            foreach (var s in sequences)
+                if (s.Length > maxnum)
+                    maxnum = s.Length;
+
+            return maxnum;
+        }
+    }
+}
