c892bc3c779a9fd5e5ef748f8cbe5ad29027601c
NPerederiy <nick.perederiy@gmail.com>
2018-10-29 21:15:19 +0200

    Added algorithm #4
    Algorithm for finding all prime numbers up to any given limit

---
+++ GetPrimeNumbers/Program.cs
@@ -0,0 +1,56 @@
+﻿using System.IO;
+using System.Collections.Generic;
+
+namespace Eratosthenes
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var primeNumbers = SieveOfEratosthenes.GetPrimeNumbers(99999);
+            var output = "";
+
+            foreach (var prime in primeNumbers)
+            {
+                output += $"{prime} ";
+            }
+
+            File.WriteAllText("OUTPUT.txt", output, System.Text.Encoding.UTF8);
+        }
+    }
+
+    public class SieveOfEratosthenes
+    {
+        private static List<int> primeNumbers;
+
+        public static int[] GetPrimeNumbers(int limit)
+        {
+            primeNumbers = new List<int>();
+            for (int i = 1; i < limit; i++)
+                primeNumbers.Add(i);
+            Start();
+            return primeNumbers.ToArray();
+        }
+
+        private static void Start()
+        {
+            int i = 1;
+            while (i < primeNumbers.Count)
+            {
+                Step(primeNumbers[i], i);
+                i++;
+            }
+        }
+
+        private static void Step(int prime, int startFrom)
+        {
+            int i = startFrom + 1;
+            while (i < primeNumbers.Count)
+                if (primeNumbers[i] % prime == 0)
+                {
+                    primeNumbers.RemoveAt(i);
+                }
+                else i++;
+        }
+    }
+}
--- README.md
+++ README.md
@@ -4,4 +4,5 @@ Name | Description | Input | Output
 -----| ----------- | ----- | ------
 GetAllDivisors | Algorithm for searching all integer divisors of a number | Number | Array of integer divisors
 GetLongestSequenceLength | Algorithm for finding the length of the longest continuous chain of ones in a sequence of zeros and ones | Sequence of zeros and ones | The length of the longest continuous chain of ones
+GetPrimeNumbers (Sieve of Eratosthenes) | Algorithm for finding all prime numbers up to any given limit | Number | Array of prime numbers
 IsFibonacci | The algorithm determines whether a given number is a Fibonacci number | Number | Boolean value 
